a0f8b445337b65a1627e609e1de0739e9f20b09e
paweljurczyszyn <108095627+paweljurczyszyn@users.noreply.github.com>
2023-11-26 22:47:17 +0100

    Delete DuoTravel_v14 directory

--- DuoTravel_v14/app/src/main/java/pl/duotravel/duotravel_v14/ShowToast.kt
+++
@@ -1,8 +0,0 @@
-package pl.duotravel.duotravel_v14
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String){
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
